8908e8bede972878224f998ff6448af87c5c1294
Ahrkylien <henkvgrootheest@gmail.com>
2025-11-01 17:15:27 +0100

    add alaris prime with mobile spawnpoint for rebels

--- build/make.py
+++ build/make.py
@@ -2,17 +2,19 @@ import os
 import sys
 from BF1942_Extraction_Readout_Scripts.refractor_flat_archive import RefractorFlatArchive
 
-map_name = "GC_Bespin_Night"
+maps_folder = "../src/bf1942/levels"
+map_names = [name for name in os.listdir(maps_folder)]
 
-client_file_path = f"client/{map_name}.rfa"
-if not os.path.isfile(client_file_path):
-    print(f"Error: Please add the client file \"{map_name}.rfa\" first to this folder: \:build\client\".")
-    sys.exit()
+for map_name in map_names:
+    client_file_path = f"client/{map_name}.rfa"
+    if not os.path.isfile(client_file_path):
+        print(f"Error: Please add the client file \"{map_name}.rfa\" first to this folder: \"build\\client\".")
+        sys.exit()
 
-rfa = RefractorFlatArchive(client_file_path)
-rfa.delete_all_non_server_files()
-rfa.add_directory(f"../src/bf1942/levels/{map_name}", "../src")
+    rfa = RefractorFlatArchive(client_file_path)
+    rfa.delete_all_non_server_files()
+    rfa.add_directory(f"{maps_folder}/{map_name}", "../src")
 
-if not os.path.exists("server"):
-    os.makedirs("server")
-rfa.write(f"server{os.sep}{map_name.lower()}.rfa")
+    if not os.path.exists("server"):
+        os.makedirs("server")
+    rfa.write(f"server{os.sep}{map_name.lower()}.rfa")
